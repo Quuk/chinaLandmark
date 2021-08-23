7c31861d20ad47bac08821fb8146c9c9c111dfca
tcyeee <tcyeee@outlook.com>
2021-08-23 15:05:27 +0800

    add home page

--- chinaLandmark/home.swift
+++ chinaLandmark/home.swift
@@ -19,10 +19,52 @@ struct home: View {
     var body: some View {
         NavigationView {
             List{
+                // 首页头图
                 Home_title_img()
                 
-                
+                // 建筑分类列表
+                ForEach(categories.keys.sorted(), id: \.self){ item in
+                    Landmark_pack(key: item,group: self.categories[item]!)
+                }
             }.navigationBarTitle(Text("精选"))
+        }
+    }
+}
+
+// 单组landmark
+struct Landmark_pack: View{
+    let key: String
+    let group: [Landmark]
+
+    var body: some View {
+        VStack(alignment:.leading){
+            Text(key).font(.headline)
+            ScrollView(.horizontal,showsIndicators:false){
+                HStack(spacing: 15){
+                    ForEach(group){ city in
+                        Home_landmark_cell(cell: city)
+                    }
+                }
+            }
+        }
+        .padding(.leading,0)
+        .padding([.top,.bottom])
+    }
+}
+
+
+// 单个landmarkCell
+struct Home_landmark_cell: View{
+    let cell: Landmark
+    
+    var body: some View{
+        VStack(alignment: .leading){
+            Image(cell.imageName)
+                .resizable()
+                .frame(width: 155, height: 155, alignment: .center)
+                .cornerRadius(5)
+            Text(cell.name)
+                .font(.caption)
         }
     }
 }
@@ -37,7 +79,7 @@ private struct Home_title_img: View{
             .frame(height: 170)
             .clipped()
             .listRowInsets(EdgeInsets())
-        }
+    }
 }
 
 
